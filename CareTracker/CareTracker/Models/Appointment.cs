15dddd1a2ab282b4d0bf4ba3ed47af24ca867e8e
Ray Medrano <rmbw74@gmail.com>
2018-03-07 10:27:49 -0600

    created models

---
+++ CareTracker/CareTracker/Models/Appointment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CareTracker.Models
+{
+    public class Appointment
+    {
+        [Key]
+        public int AppointmentId { get; set; }
+
+        [Required]
+        public DateTime AppointmentDate { get; set; }
+
+        [Required]
+        public DateTime AppointmentTime { get; set; }
+
+        [Required]
+        public string AppointmentReason { get; set; }
+
+        [Required]
+        public string AppointmentAddress { get; set; }
+
+       
+        public string AppointmentPhoneNumber { get; set; }
+
+        public string AppointmentNotes { get; set; }
+
+        public virtual Dependent Dependent { get; set; }
+
+        public virtual Doctor Doctor { get; set; }
+    }
+}
---
+++ CareTracker/CareTracker/Models/Dependent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CareTracker.Models
+{
+    public class Dependent
+    {
+        [Key]
+        public int DependentId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime Birthday { get; set; }
+
+        [Required]
+        public string SocialSecurityNumber { get; set; }
+
+        public string DependentNotes { get; set; }
+        
+
+        //forign key connections
+
+        public virtual ICollection<Appointment> Appointments { get; set; }
+        public virtual ICollection<DependentDoctor> DependentDoctors { get; set; }
+        public virtual ICollection<DependentUser> DependentUsers { get; set; }
+        public virtual ICollection<Prescription> Prescriptions { get; set; }
+    }
+}
