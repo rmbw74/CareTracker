34034f369cbdf6302f502031b0d9c246da04876d
Ray Medrano <rmbw74@gmail.com>
2018-03-08 14:33:33 -0600

    have initial dependents showing on homepage

---
+++ CareTracker/CareTracker/Models/DependentViewModels/ViewDependentsHomePageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CareTracker.Models.DependentViewModels
+{
+    public class ViewDependentsHomePageViewModel
+    {
+        public ICollection<UserDependent> UserDependents { get; set; }
+    }
+}
