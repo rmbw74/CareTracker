fc7a7985d5936e626f4e6582a36e8381a2b6e487
Ray Medrano <rmbw74@gmail.com>
2018-03-12 12:45:20 -0500

    added DependentSummaryViewModel

---
+++ CareTracker/CareTracker/Models/DependentViewModels/DependentSummaryViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CareTracker.Models.DependentViewModels
+{
+    public class DependentSummaryViewModel
+    {
+        public int DependentId { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+    
+        public DateTime Birthday { get; set; }
+
+        public int Age { get; set; }
+
+        [Display(Name = "Social Security Number")]
+        public string SocialSecurityNumber { get; set; }
+
+        [Display(Name = "Notes")]
+        public string DependentNotes { get; set; }
+
+        public ICollection<Appointment> Appointments { get; set; }
+        public ICollection<Prescription> Prescriptions { get; set; }
+
+    }
+}
