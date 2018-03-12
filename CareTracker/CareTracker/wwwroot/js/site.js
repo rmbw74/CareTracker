bb031f2fb1a8a6dc4c7640df400b3c8233b5252c
Ray Medrano <rmbw74@gmail.com>
2018-03-12 11:32:00 -0500

    added event listener for add dependents button

--- CareTracker/CareTracker/wwwroot/js/site.js
+++ CareTracker/CareTracker/wwwroot/js/site.js
@@ -1,1 +1,5 @@
-﻿// Write your JavaScript code.
+﻿$("#AddDependentButton").on("click", evt => {
+    console.log("You Clicked")
+    evt.preventDefault()
+    window.location = `/Dependents/Create`
+})
