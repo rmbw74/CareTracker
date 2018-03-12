c2a65598b3e574e617bf7f204b9248b2a13d3876
Ray Medrano <rmbw74@gmail.com>
2018-03-12 11:39:05 -0500

    added comments for event listener

--- CareTracker/CareTracker/wwwroot/js/site.js
+++ CareTracker/CareTracker/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿$("#AddDependentButton").on("click", evt => {
+﻿
+//This eventlistener fires off when a user clicks on the add dependents button and redirects to the Dependents Create URL
+$("#AddDependentButton").on("click", evt => {
     console.log("You Clicked")
     evt.preventDefault()
     window.location = `/Dependents/Create`
