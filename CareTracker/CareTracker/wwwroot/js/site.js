c3e173e327b8f64fef2722d42ea9c20d5dc5bad2
Ray Medrano <rmbw74@gmail.com>
2018-03-13 13:44:59 -0500

    added eventlistener to dependent on home screen to access summary view

--- CareTracker/CareTracker/wwwroot/js/site.js
+++ CareTracker/CareTracker/wwwroot/js/site.js
@@ -5,3 +5,15 @@ $("#AddDependentButton").on("click", evt => {
     evt.preventDefault()
     window.location = `/Dependents/Create`
 })
+
+
+//This eventlistener fires off when a user clicks on a dependent, and will show the dependent summary screen
+$(".UserDependentList__Dependent").on("click", evt => {
+
+    //split the event target id and grab the dependent id number
+    const DependentId = evt.target.id.split("--")[1]
+    console.log("You clicked on ", DependentId )
+    //change window locations using the dependentId to show summary for dependent
+    window.location = `/Summary/Show/${DependentId}`
+    
+})
