fd69ece53ba2673a2ec2c81ffd0c3373175b0ac5
Ray Medrano <rmbw74@gmail.com>
2018-03-15 18:57:14 -0500

    event listener added to view doctors button

--- CareTracker/CareTracker/wwwroot/js/site.js
+++ CareTracker/CareTracker/wwwroot/js/site.js
@@ -12,8 +12,32 @@ $(".UserDependentList__Dependent").on("click", evt => {
 
     //split the event target id and grab the dependent id number
     const DependentId = evt.target.id.split("--")[1]
-    console.log("You clicked on ", DependentId )
-    //change window locations using the dependentId to show summary for dependent
-    window.location = `/Summary/Show/${DependentId}`
+    if (DependentId) {
+        console.log("You clicked on ", DependentId)
+        //change window locations using the dependentId to show summary for dependent
+        window.location = `/Summary/Show/${DependentId}`
+    }
+        
+    
+    
+})
+
+//This eventlistener fires off when a user clicks on the Show All Doctors button on the summary page and directs the browser to 
+// the doctors/showall page
+
+$("#AppointmentActionBar").on("click", evt => {
+
+    //split the target id and grab the dependentid
+    const DependentId = evt.target.id.split("--")[1]
+    const Target = evt.target.classList
+    console.log(Target)
+    console.log(evt.target.id)
+    if (DependentId) {
+        console.log("We made it")
+        
+        window.location = `/Doctors/DependentDoctors/${DependentId}`
+    }
+    
+    
     
 })
