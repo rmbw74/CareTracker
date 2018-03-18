aa497edc6b1ecba0c337f05a9b7b038ea27b69f1
Ray Medrano <rmbw74@gmail.com>
2018-03-18 13:58:23 -0500

    can view all appointments for dependnents by routing to appointments - allappointments

---
+++ CareTracker/CareTracker/Models/AppointmentViewModels/AllAppointmentsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CareTracker.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CareTracker.Models.AppointmentViewModels
+{
+    public class AllAppointmentsViewModel
+    {
+
+        public IEnumerable<DependentAppointment> Appointments { get; set; }
+
+       
+        
+    }
+}
---
+++ CareTracker/CareTracker/Models/DependentAppointment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CareTracker.Models
+{
+    public class DependentAppointment
+    {
+        public int AppointmentId { get; set; }
+        public int DependentId { get; set; }
+        public int DoctorId { get; set; }
+        public string DoctorFirstName { get; set; }
+        public string DoctorLastName { get; set; }
+        public string DependentFirstName { get; set; }
+        public string DependentLastName { get; set; }
+        [Display(Name = "Date")]
+        [DataType(DataType.Date)]
+        public DateTime AppointmentDate { get; set; }
+        [Display(Name = "Time")]
+        [DataType(DataType.Time)]
+        public DateTime AppointmentTime { get; set; }
+        public string AppointmentReason { get; set; }
+        public string AppointmentAddress { get; set; }
+        public string AppointmentPhone { get; set; }
+        public string AppointmentNotes { get; set; }
+
+    }
+}
