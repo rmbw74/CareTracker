436648a2b96881582f5c631294dee49ba91066db
Ray Medrano <rmbw74@gmail.com>
2018-03-19 10:14:38 -0500

    added new view model

--- CareTracker/CareTracker/Models/PrescriptionViewModels/DependentAllPrescriptionsViewModel.cs
+++ CareTracker/CareTracker/Models/PrescriptionViewModels/DependentAllPrescriptionsViewModel.cs
@@ -8,39 +8,8 @@ namespace CareTracker.Models.PrescriptionViewModels
 {
     public class DependentAllPrescriptionsViewModel
     {
-        public int PrescriptionId { get; set; }
-
-
-        [Display(Name = "Drug Name")]
-        public string DrugName { get; set; }
-
-
-        public string Dosage { get; set; }
-
-        public string Frequency { get; set; }
-
-        [Display(Name = "Pharmacy Address")]
-        public string PharmacyAddress { get; set; }
-
-        [DataType(DataType.PhoneNumber)]
-        [Display(Name = "Pharmacy Phone Number")]
-        public string PharmacyPhoneNumber { get; set; }
-
-        [Display(Name = "Active?")]
-        public bool PrescriptionActive { get; set; }
-
-        public int DependentId { get; set; }
-        [Display(Name = "Dependent First Name")]
-        public string DependentFirstName { get; set; }
-        [Display(Name = "Dependent Last Name")]
-        public string DependentLastName { get; set; }
-
-        public int DoctorId { get; set; }
-        [Display(Name = "Doctor")]
-        public string DoctorLastName { get; set; }
-
-        public string DoctorFirstName { get; set; }
-
+       
+        public ICollection<DependentPrescription> DependentPrescriptions { get; set; }
 
     }
 
