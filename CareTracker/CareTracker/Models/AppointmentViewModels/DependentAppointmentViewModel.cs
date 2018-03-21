85ca8afd642b318ee66b0681f5f764d18dd15265
Ray Medrano <rmbw74@gmail.com>
2018-03-21 17:01:49 -0500

    added DependentAppointmentsViewModel

---
+++ CareTracker/CareTracker/Models/AppointmentViewModels/DependentAppointmentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CareTracker.Models.AppointmentViewModels
+{
+    public class DependentAppointmentViewModel
+
+    {
+        
+        public ICollection<DependentAppointment> Appointments { get; set; }
+    }
+}
