9633457ad0fb43bf98a4348fbf49cebbe3ede57f
Ray Medrano <rmbw74@gmail.com>
2018-03-22 09:04:27 -0500

    Added custom view models for Appointment and Prescription Detail pages

---
+++ CareTracker/CareTracker/Models/AppointmentViewModels/AppointmentDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CareTracker.Models.AppointmentViewModels
+{
+    public class AppointmentDetailsViewModel
+    {
+        public Appointment Appointment { get; set; }
+    }
+}
