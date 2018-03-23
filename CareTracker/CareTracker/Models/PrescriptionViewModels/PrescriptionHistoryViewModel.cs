5af387e3aa81457d76abad5d7236a6069b6929ea
Ray Medrano <rmbw74@gmail.com>
2018-03-22 19:46:11 -0500

    added PrescriptionHistoryViewModel

---
+++ CareTracker/CareTracker/Models/PrescriptionViewModels/PrescriptionHistoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CareTracker.Models.PrescriptionViewModels
+{
+    public class PrescriptionHistoryViewModel
+    {
+    }
+}
